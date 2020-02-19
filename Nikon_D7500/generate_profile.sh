d575c9ca9a31fe6235011dbe85a251441fc32f9f
Johan Splinter <26428407+kwazel@users.noreply.github.com>
2020-02-19 20:51:25 +0100

    ICC profile for Nikon D7500

---
+++ Nikon_D7500/generate_profile.sh
@@ -0,0 +1,3 @@
+scanin -v -p -a -dipnoa Nikon_D7500_ISO100.tif ~/Downloads/Photo/Argyll_V2.1.2/ref/it8.cht ../R180730.txt
+colprof -v -A "Nikon" -M "D7500" -D "Nikon D7500 iso 100" -C "Copyright (c) Johan Splinter 2020" -q l -a m -u Nikon_D7500_ISO100
+
