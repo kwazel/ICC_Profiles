5425d224f710e58e674d533200383f01f0fa791e
Johan Splinter <26428407+kwazel@users.noreply.github.com>
2020-02-19 20:56:42 +0100

    ICC profile for Lumix LX100

---
+++ Lumix_LX100/generate_profile.sh
@@ -0,0 +1,3 @@
+scanin -v -p -a -dipnoa Lumix_LX100_ISO200.tif ~/Downloads/Photo/Argyll_V2.1.2/ref/it8.cht ../R180730.txt
+colprof -v -A "Panasonic" -M "DMC-LX100" -D "LX100 iso 200" -C "Copyright (c) Johan Splinter 2020" -q l -a m -u Lumix_LX100_ISO200
+
